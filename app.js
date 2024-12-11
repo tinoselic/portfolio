670b08b4875717912561b4317dbdd3197387eae5
Tino Selic <tino.selic@gmail.com>
2024-12-11 16:07:16 +0100

    Index.html css updated.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+const home = document.getElementById('home');
 const main = document.querySelector('main');
 const contact = document.getElementById('contact');
 const footer = document.getElementById('copyright');
@@ -17,42 +18,44 @@ function showNav() {
 	mainMenu.style.display = 'block';
 	openMenu.style.display = 'none';
 	main.style.display = 'none';
+	home.style.backgroundColor = 'rgba(0, 0, 0, 1)';
 }
 
 function closeNav() {
 	mainMenu.style.display = 'none';
 	openMenu.style.display = 'block';
 	main.style.display = 'block';
+	home.style.backgroundColor = 'rgba(0, 0, 0, 0)';
 }
 
 // Contact/Email button interaction
 contact.addEventListener('click', function () {
 	const mailtoLink = 'mailto:tino.selic@gmail.com}';
-    window.location.href = mailtoLink;
+	window.location.href = mailtoLink;
 });
 
 
 // Highlight Project on Scroll
 // Options for the Intersection Observer
 const options = {
-    root: null, // Sets the viewport as the root
-    rootMargin: '0px',
-    threshold: 1 // Triggers when 100% of the box is visible
+	root: null, // Sets the viewport as the root
+	rootMargin: '0px',
+	threshold: 1 // Triggers when 100% of the box is visible
 };
 
 // Callback function for the observer
 function highlightOnScroll(entries, observer) {
-    if (window.innerWidth <= 796) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // Add the highlight class when the element is in view
-                entry.target.classList.add('highlight');
-            } else {
-                // Optionally, remove the highlight when it’s out of view
-                entry.target.classList.remove('highlight');
-            }
-        });
-    }
+	if (window.innerWidth <= 796) {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				// Add the highlight class when the element is in view
+				entry.target.classList.add('highlight');
+			} else {
+				// Optionally, remove the highlight when it’s out of view
+				entry.target.classList.remove('highlight');
+			}
+		});
+	}
 }
 
 // Create the observer with the callback and options
